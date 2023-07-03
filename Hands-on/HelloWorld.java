d904df17f6986eb07a0bbf09c15867e62591765a
Jenish012 <137861557+Jenish012@users.noreply.github.com>
2023-07-03 12:22:18 +0530

    Add files via upload

---
+++ Hands-on/HelloWorld.java
@@ -0,0 +1,13 @@
+//To print HELLO WORLD
+class HelloWorld
+{
+public static void main(String[] agrs)
+{
+	System.out.println("Hello World");
+}
+}
+
+
+//In Notepad
+//javac HelloWorld.java
+//java HelloWord
